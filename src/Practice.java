f46a0b5fb296bb3d93fabe747a6efe242a1f97d4
rashsing <singhrashmi.0906@gmail.com>
2022-05-10 20:49:46 +0530

    initial

---
+++ src/Practice.java
@@ -0,0 +1,13 @@
+public class Practice {
+    void run(int x) {
+    System.out.println("inside funtion");
+    }
+
+
+
+
+    public static void main(String[] s) {
+        Practice p = new Practice();
+        p.run(4);
+    }
+}
